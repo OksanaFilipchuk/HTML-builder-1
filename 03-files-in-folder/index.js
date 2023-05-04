4c1d51b7e1ea1db866b691d1e51b371005fd5420
Oksana Filipchuk <oksanafilipchuk@MacBook-Air-Oksana.local>
2023-05-05 01:07:35 +0300

    feat: add 1-3 solutions

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fsPromise = require("fs/promises");
+const fs = require("fs");
+const path = require("path");
+
+fsPromise.readdir("secret-folder", { withFileTypes: true }).then((files) => {
+  let array = [];
+  files.forEach((el) => {
+    let obj = {
+      name: el.name,
+      isDir: el.isDirectory(),
+      ext: el.isDirectory() ? "" : path.extname(el.name),
+    };
+    array.push(obj);
+  });
+  console.table(array);
+});
